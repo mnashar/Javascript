042200d990c1e7b2980a28d6863d776468196812
your name here <msn@aucegypt.edu>
2020-05-24 11:20:03 -0400

    mysteryScoping1

--- part1.js
+++ part1.js
@@ -9,3 +9,13 @@ function mysteryScoping1() {
 
 mysteryScoping1(); // in block, in block
 
+mysteryScoping2(); // in block, out of block
+
+function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+        var x = 'in block';
+        console.log(x);
+    }
+    console.log(x);
+}
