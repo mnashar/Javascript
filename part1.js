5b298a3478a089182db438aceb56a76ed107fc8d
your name here <msn@aucegypt.edu>
2020-05-25 01:09:17 -0400

    mysteryScoping1

--- part1.js
+++ part1.js
@@ -30,3 +30,15 @@ function mysteryScoping4() {
     }
     console.log(x);
 }
+
+mysteryScoping4(); // in block, out of block
+
+function mysteryScoping5() {
+    let x = 'out of block';
+    if (true) {
+        let x = 'in block';
+        console.log(x);
+    }
+    let x = 'out of block again';
+    console.log(x);
+}
